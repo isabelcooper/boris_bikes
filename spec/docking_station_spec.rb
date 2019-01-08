4107ccaad8f6cd3f94fcbf51521788464e06ca3c
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-08 15:43:43 +0000

    DockingStation now creates Bike class objects

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
-require 'docking_station.rb'
+require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
+
+  it 'expects release_bike equals a new bike' do
+  expect(subject.release_bike.working?).to eq true
+  end
 
 end
