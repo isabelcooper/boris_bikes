8ad13f8846b1bf133cc6cbf9261b97d42bad30d4
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-01-10 13:48:40 +0000

    refactored all tests for cleaner code - created instances of new bike and new docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,27 @@
 require 'docking_station'
 #require 'bike'
-
 describe DockingStation do
-
-  it { is_expected.to respond_to :release_bike }
-
-  it 'expects release_bike to return a bike when a (working) bike is at the station' do
-    station_with_bike = DockingStation.new
-    bike_at_station = Bike.new
-    station_with_bike.dock(bike_at_station)
-    expect(station_with_bike.release_bike).to eq(bike_at_station)
+  before(:each) do
+    @docking_station = DockingStation.new
+    @newbike = Bike.new
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
-
+  it 'expects release_bike to return a bike when a (working) bike is at the station' do
+    @docking_station.dock(@newbike)
+    expect(@docking_station.release_bike).to eq(@newbike)
+  end
+  
   it 'expects bike attribute of station to equal docked bike' do
-    unique_bike = Bike.new
-    subject.dock(unique_bike)
-    expect(subject.docked_bikes).to include(unique_bike)
+    @docking_station.dock(@newbike)
+    expect(@docking_station.docked_bikes).to include(@newbike)
   end
 
   it 'expects new docking station with no bikes to raise error when releasing a bike' do
-    expect { DockingStation.new.release_bike }.to raise_error("There are no bikes")
+    expect { @docking_station.release_bike }.to raise_error("There are no bikes")
   end
 
   it 'will raise an error if attempting to dock a bike in a full station' do
-    occupied_dock = DockingStation.new()
-    20.times { occupied_dock.dock Bike.new }
-    expect { occupied_dock.dock("21st Bike") }.to raise_error("Station is full")
+    20.times { @docking_station.dock @newbike }
+    expect { @docking_station.dock("21st bike") }.to raise_error("Station is full")
   end
 end
